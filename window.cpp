3ad847fa212085366f1e4699e57d8a3a40b03d99
GLOBIP <kamiliasd@interia.eu>
2026-07-25 18:38:23 +0200

    dzial do gory

--- window.cpp
+++ window.cpp
@@ -3,6 +3,7 @@
 #include "server.h"
 #include <cstring>
 #include <fstream>
+#include <iostream>
 #include <unistd.h>
 #include <vector>
 void checkRead(char &Who, std::string zdanie) {
@@ -53,8 +54,13 @@ void windowFileRelated::drawOnScreen(int side, WindowParts *MyParts,
   int right = MyParts->startx + MyParts->width;
   int middlescr = MyParts->startx + MyParts->width / 2;
   for (;;) {
+    while (starty + 2 > MyParts->height) { // some magic way MyParts.height is
+                                           // starting point of text tab lmao
+      starty -= 2;
+    }
     if (side + tekst.size() >= right) {
       side--;
+      continue;
     } else if (side < middlescr && primarySide > middlescr) {
       starty++;
       side = middlescr + 2;
@@ -62,9 +68,10 @@ void windowFileRelated::drawOnScreen(int side, WindowParts *MyParts,
                                         // ŹLE ROZDZIELAMY Z MIDDLE
       tekst2 = tekst.substr(0, middle); // TO TEZ SLABE
       tekst.erase(0, middle);           // TO JEST SLABE
-      tesktWriteVec.push_back(tekst2);
-      sideVec.push_back(side);
-      startyVec.push_back(starty);
+                                        /*
+                                  tesktWriteVec.push_back(tekst2);
+                                  sideVec.push_back(side);
+                                  startyVec.push_back(starty);*/
 
     } else if (side + tekst.size() > middlescr && primarySide < middlescr) {
       starty++;
@@ -73,9 +80,10 @@ void windowFileRelated::drawOnScreen(int side, WindowParts *MyParts,
                                         // ŹLE ROZDZIELAMY Z MIDDLE
       tekst2 = tekst.substr(0, middle); // TO TEZ SLABE
       tekst.erase(0, middle);           // TO JEST SLABE
-      tesktWriteVec.push_back(tekst2);
-      sideVec.push_back(side);
-      startyVec.push_back(starty);
+                                        /*
+                                  tesktWriteVec.push_back(tekst2);
+                                  sideVec.push_back(side);
+                                  startyVec.push_back(starty);*/
 
     } else {
       tesktWriteVec.push_back(tekst);
@@ -83,6 +91,14 @@ void windowFileRelated::drawOnScreen(int side, WindowParts *MyParts,
       startyVec.push_back(starty);
       break;
     }
+
+    // push back always because if it:
+    // 1. Is too right it slides left and continuse - begin on start
+    // 2. if all ok it breaks
+
+    tesktWriteVec.push_back(tekst2);
+    sideVec.push_back(side);
+    startyVec.push_back(starty);
   }
   for (int i{}; i < tesktWriteVec.size(); i++) {
     move(startyVec.at(i), sideVec.at(i));
