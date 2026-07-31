06d2526bf584e9067e223902139da8bd88141bf1
GLOBIP <kamiliasd@interia.eu>
2026-07-31 21:56:34 +0200

    jest dobrze

--- fileChat.cpp
+++ fileChat.cpp
@@ -4,38 +4,44 @@
 #include <iostream>
 #include <string>
 void files::sendFileServer(std::string message) {
-
-  std::ofstream fileWrite("comm.txt", std::ios::app);
-  if (fileWrite.is_open()) {
-    std::cout << "OPENED";
-  } else {
-    perror("failed");
+  try {
+    std::ofstream fileWrite("comm.txt", std::ios::app);
+    if (fileWrite.is_open()) {
+      std::cout << "OPENED";
+    } else {
+      perror("failed");
+    }
+    std::string writeToFile = "Server\n";
+    time_t timestamp;
+    time(&timestamp);
+    std::string time = ctime(&timestamp);
+    writeToFile += time;
+    writeToFile += message;
+    writeToFile.push_back('\n');
+    fileWrite.write(writeToFile.c_str(), writeToFile.size());
+    fileWrite.close();
+  } catch (int i) {
+    printf("Something went wrong while writing to file ");
   }
-  std::string writeToFile = "Server\n";
-  time_t timestamp;
-  time(&timestamp);
-  std::string time = ctime(&timestamp);
-  writeToFile += time;
-  writeToFile += message;
-  writeToFile.push_back('\n');
-  fileWrite.write(writeToFile.c_str(), writeToFile.size());
-  fileWrite.close();
 }
 void files::sendFileClient(std::string message) {
-
-  std::ofstream fileWrite("comm.txt", std::ios::app);
-  if (fileWrite.is_open()) {
-    std::cout << "OPENED";
-  } else {
-    perror("failed");
+  try {
+    std::ofstream fileWrite("comm.txt", std::ios::app);
+    if (fileWrite.is_open()) {
+      std::cout << "OPENED";
+    } else {
+      perror("failed");
+    }
+    std::string writeToFile = "Client\n";
+    time_t timestamp;
+    time(&timestamp);
+    std::string time = ctime(&timestamp);
+    writeToFile += time;
+    writeToFile += message;
+    writeToFile.push_back('\n');
+    fileWrite.write(writeToFile.c_str(), writeToFile.size());
+    fileWrite.close();
+  } catch (int i) {
+    printf("Something went wrong while writing to file ");
   }
-  std::string writeToFile = "Client\n";
-  time_t timestamp;
-  time(&timestamp);
-  std::string time = ctime(&timestamp);
-  writeToFile += time;
-  writeToFile += message;
-  writeToFile.push_back('\n');
-  fileWrite.write(writeToFile.c_str(), writeToFile.size());
-  fileWrite.close();
 }
